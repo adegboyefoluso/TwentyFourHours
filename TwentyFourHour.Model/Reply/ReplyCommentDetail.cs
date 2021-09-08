e8153e6282bef2f80e24c5f3c29ee8f37123156e
adegboyefoluso <foluso.o.adegboye@gmail.com>
2021-09-08 11:34:41 -0400

    Added connection string and Reply controller

---
+++ TwentyFourHour.Model/Reply/ReplyCommentDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyFourHour.Model.Reply
+{
+    public class ReplyCommentDetail
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public List<ReplyDetail> Replies { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
