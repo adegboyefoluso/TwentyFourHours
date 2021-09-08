62004fd2c1ab7d719946214b138bca0ed0e95c1c
Otto Lutz <ottolutz00@gmail.com>
2021-09-08 11:39:46 -0400

    Added PostController and methods

---
+++ TwentyFourHour.Service/PostService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwentyFourHour.Model;
+using TwentyFourHours.Data;
+
+namespace TwentyFourHour.Service
+{
+    public class PostService
+    {
+        private readonly Guid _userId;
+
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        // Create
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    AuthorId = _userId,
+                    Title = model.Title,
+                    Text = model.Text,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        // Read
+        public IEnumerable<PostListItem> GetPosts()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Posts
+                    .Where(e => e.AuthorId == _userId)
+                    .Select(
+                        e =>
+                        new PostListItem()
+                        {
+                            PostId = e.PostId,
+                            Title = e.Title,
+                            CreatedUtc = e.CreatedUtc
+                        });
+                return query.ToArray();
+            }
+        }
+        // Read by Id
+        public PostDetail GetPostById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Posts
+                    .Single(e => e.PostId == id && e.AuthorId == _userId);
+                return
+                    new PostDetail()
+                    {
+                        PostId = entity.PostId,
+                        Title = entity.Title,
+                        Text = entity.Text,
+                        Comments = ctx.Comments.Where(c => c.PostId == id)
+                    };
+            }
+        }
+
+        // Update
+        public bool UpdatePost(PostEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Posts
+                    .Single(e => e.PostId == model.PostId && e.AuthorId == _userId);
+
+                entity.Title = model.Title;
+                entity.Text = model.Text;
+                entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+
+        // Delete
+        public bool DeletePost(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Posts
+                    .Single(e => e.PostId == id && e.AuthorId == _userId);
+
+                ctx.Posts.Remove(entity);
+                return ctx.SaveChanges() == 1;
+            };
+        }
+
+    }
+}
